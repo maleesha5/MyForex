b22f2a5f8c8327097e69ae9a8ca67635d91bdf34
maleesha5 <maleesha55@gmail.com>
2018-03-11 11:37:55 +0530

    Calculator changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,9 @@
 angular.module('myForexlkApp')
   .controller('MainCtrl', function ($scope, $uibModal, masterCurrencyList, bankList, cbRatesList, bankRatesList) {
 
+    var sl = { "country": "Sri Lanka", "currencyCode": "LKR", "flag": "LK", "name": "Sri Lankan Rupees" };
+    var us = { "country": "United States of America", "currencyCode": "USD", "flag": "US", "name": "United States Dollar" };
+
     $scope.masterCurrencyList = masterCurrencyList;
 
     $scope.bankList = bankList;
@@ -20,16 +23,15 @@ angular.module('myForexlkApp')
       return "flag-icon flag-icon-" + flag.toLowerCase();
     }
 
-    $scope.toCurrency;
-    $scope.fromCurrency = {"country":"Sri Lanka","currencyCode":"LKR","flag":"LK","name":"Sri Lankan Rupees"};
-    $scope.currencyVal = 1;
-    $scope.toAmount = 0;
-    $scope.fromAmount;
+    $scope.toCurrency = sl;
+    $scope.fromCurrency = us;
+    $scope.currencyVal = 0;
+    $scope.resultAmount = 1;
+    $scope.fromAmount = 1;
     $scope.bankSelected;
-
+    $scope.currencySymbol = "LKR";
 
     $scope.setFromCurrency = function (currency) {
-      debugger
       $scope.fromCurrency = currency;
       $scope.fromCurrencyOnChange();
     }
@@ -39,34 +41,57 @@ angular.module('myForexlkApp')
       $scope.toCurrencyOnChange();
     }
 
-    /*$scope.toCurrencyOnChange = function () {
+    $scope.calculateResultAmount = function () {
+      if ($scope.fromCurrency.currencyCode != "LKR") {
+        $scope.resultAmount = $scope.fromAmount * $scope.currencyVal;
+        $scope.currencySymbol = $scope.toCurrency.currencyCode;
+      } else {
+        $scope.resultAmount = $scope.fromAmount / $scope.currencyVal;
+        $scope.currencySymbol = $scope.toCurrency.currencyCode;
+      }
+
+    }
+
+    $scope.toCurrencyOnChange = function () {
+      var notFoundRate = true;
       if ($scope.fromCurrency && !$scope.bankSelected) {
 
         angular.forEach($scope.cbRatesList, function (value, key) {
-          
-          if (value.currerncyMaster.currencyCode == $scope.fromCurrency.currencyCode) {
+          if (value.currerncyMaster.currencyCode == $scope.toCurrency.currencyCode) {
             $scope.currencyVal = value.sellRate;
+            notFoundRate = false;
           }
         });
-        $scope.fromAmount = $scope.toAmount * $scope.currencyVal;
+        //$scope.resultAmount = $scope.fromAmount / $scope.currencyVal;
       }
-    }*/
+      if (notFoundRate) {
+        $scope.currencyVal = 0;
+      }
+      $scope.calculateResultAmount();
+    }
 
     $scope.fromCurrencyOnChange = function () {
+      var notFoundRate = true;
       if ($scope.fromCurrency && $scope.bankSelected) {
-        debugger
         $scope.currencyVal = $scope.getBankBuyRateForCurrency($scope.bankSelected, $scope.fromCurrency);
-        $scope.toAmount = $scope.fromAmount * $scope.currencyVal;
+        //$scope.resultAmount = $scope.fromAmount * $scope.currencyVal;
+        notFoundRate = false;
 
       } else if ($scope.fromCurrency && !$scope.bankSelected) {
         angular.forEach($scope.cbRatesList, function (value, key) {
 
           if (value.currerncyMaster.currencyCode == $scope.fromCurrency.currencyCode) {
             $scope.currencyVal = value.buyRate;
+            notFoundRate = false;
+
           }
         });
-        $scope.toAmount = $scope.fromAmount * $scope.currencyVal;
+        // $scope.resultAmount = $scope.fromAmount * $scope.currencyVal;
       }
+      if (notFoundRate) {
+        $scope.currencyVal = 0;
+      }
+      $scope.calculateResultAmount();
     }
 
     $scope.getRateForCurrency = function (currencyCode) {
@@ -75,6 +100,7 @@ angular.module('myForexlkApp')
     $scope.setBankSelected = function (bank) {
       $scope.bankSelected = bank;
       $scope.fromCurrencyOnChange();
+      $scope.calculateResultAmount();
     }
 
     $scope.getBankSellRateForCurrency = function (bank, currency) {
@@ -102,4 +128,7 @@ angular.module('myForexlkApp')
       });
       return currentBuyRate;
     }
+
+    $scope.fromCurrencyOnChange();
+
   });
